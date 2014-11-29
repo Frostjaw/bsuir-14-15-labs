e727460ee628f9ba18845f1192cdb48b687c843e
Denis Tuskenis <illuzive13@gmail.com>
2014-11-29 13:44:35 +0300

    [ZIIVS] [3] done

---
+++ ZIIVS/Lab3/Lab3/MainForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Lab3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.encodeButton = new System.Windows.Forms.Button();
+            this.decodeButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.inputTextLabel = new System.Windows.Forms.Label();
+            this.encodedTextLabel = new System.Windows.Forms.Label();
+            this.decodedTextLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.encodedTextBox = new System.Windows.Forms.TextBox();
+            this.decodedTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // encodeButton
+            // 
+            this.encodeButton.Location = new System.Drawing.Point(338, 256);
+            this.encodeButton.Name = "encodeButton";
+            this.encodeButton.Size = new System.Drawing.Size(49, 29);
+            this.encodeButton.TabIndex = 3;
+            this.encodeButton.Text = ">>";
+            this.encodeButton.UseVisualStyleBackColor = true;
+            this.encodeButton.Click += new System.EventHandler(this.encodeButton_Click);
+            // 
+            // decodeButton
+            // 
+            this.decodeButton.Location = new System.Drawing.Point(719, 256);
+            this.decodeButton.Name = "decodeButton";
+            this.decodeButton.Size = new System.Drawing.Size(49, 29);
+            this.decodeButton.TabIndex = 4;
+            this.decodeButton.Text = ">>";
+            this.decodeButton.UseVisualStyleBackColor = true;
+            this.decodeButton.Click += new System.EventHandler(this.decodeButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(774, 506);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(320, 23);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // inputTextLabel
+            // 
+            this.inputTextLabel.AutoSize = true;
+            this.inputTextLabel.Location = new System.Drawing.Point(12, 10);
+            this.inputTextLabel.Name = "inputTextLabel";
+            this.inputTextLabel.Size = new System.Drawing.Size(78, 13);
+            this.inputTextLabel.TabIndex = 6;
+            this.inputTextLabel.Text = "Input text: <...>";
+            // 
+            // encodedTextLabel
+            // 
+            this.encodedTextLabel.AutoSize = true;
+            this.encodedTextLabel.Location = new System.Drawing.Point(390, 10);
+            this.encodedTextLabel.Name = "encodedTextLabel";
+            this.encodedTextLabel.Size = new System.Drawing.Size(97, 13);
+            this.encodedTextLabel.TabIndex = 7;
+            this.encodedTextLabel.Text = "Encoded text: <...>";
+            // 
+            // decodedTextLabel
+            // 
+            this.decodedTextLabel.AutoSize = true;
+            this.decodedTextLabel.Location = new System.Drawing.Point(771, 10);
+            this.decodedTextLabel.Name = "decodedTextLabel";
+            this.decodedTextLabel.Size = new System.Drawing.Size(98, 13);
+            this.decodedTextLabel.TabIndex = 8;
+            this.decodedTextLabel.Text = "Decoded text: <...>";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column3,
+            this.Column2});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(393, 256);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(320, 273);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 15F;
+            this.Column1.HeaderText = "Sym";
+            this.Column1.Name = "Column1";
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 40F;
+            this.Column3.HeaderText = "Code";
+            this.Column3.Name = "Column3";
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 45F;
+            this.Column2.HeaderText = "Frequency";
+            this.Column2.Name = "Column2";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 26);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ReadOnly = true;
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTextBox.Size = new System.Drawing.Size(320, 503);
+            this.inputTextBox.TabIndex = 10;
+            // 
+            // encodedTextBox
+            // 
+            this.encodedTextBox.Location = new System.Drawing.Point(393, 26);
+            this.encodedTextBox.MaxLength = 0;
+            this.encodedTextBox.Multiline = true;
+            this.encodedTextBox.Name = "encodedTextBox";
+            this.encodedTextBox.ReadOnly = true;
+            this.encodedTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.encodedTextBox.Size = new System.Drawing.Size(320, 224);
+            this.encodedTextBox.TabIndex = 11;
+            // 
+            // decodedTextBox
+            // 
+            this.decodedTextBox.Location = new System.Drawing.Point(774, 26);
+            this.decodedTextBox.Multiline = true;
+            this.decodedTextBox.Name = "decodedTextBox";
+            this.decodedTextBox.ReadOnly = true;
+            this.decodedTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.decodedTextBox.Size = new System.Drawing.Size(320, 474);
+            this.decodedTextBox.TabIndex = 12;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 543);
+            this.Controls.Add(this.decodedTextBox);
+            this.Controls.Add(this.encodedTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.decodedTextLabel);
+            this.Controls.Add(this.encodedTextLabel);
+            this.Controls.Add(this.inputTextLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.decodeButton);
+            this.Controls.Add(this.encodeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encodeButton;
+        private System.Windows.Forms.Button decodeButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label inputTextLabel;
+        private System.Windows.Forms.Label encodedTextLabel;
+        private System.Windows.Forms.Label decodedTextLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox encodedTextBox;
+        private System.Windows.Forms.TextBox decodedTextBox;
+    }
+}
+
---
+++ ZIIVS/Lab3/Lab3/MainForm.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lab3
+{
+    public partial class MainForm : Form
+    {
+        const String divider = "01";
+        const String inputFilePath = "input.txt";
+
+        String[] inputText;
+        String[] encodedText;
+        String[] decodedText;
+        Int32 inputSize;
+
+        Dictionary<Char, Double> frequencies;
+        Dictionary<Char, String> codingTable;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            encodedText = null;
+            decodedText = null;
+            frequencies = null;
+            codingTable = null;
+            inputText = File.ReadAllLines(inputFilePath);
+            inputSize = 0;
+
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                inputSize += (inputText[i].Length == 0) ? 1 : inputText[i].Length;
+            }
+
+            inputTextBox.Lines = inputText;
+
+            dataGridView1.Rows.Clear();
+
+            inputTextLabel.Text = "Input text: " + inputSize + " bytes";
+            encodedTextLabel.Text = "Encoded text: ";
+            decodedTextLabel.Text = "Decoded text: ";
+            encodedTextBox.Clear();
+            decodedTextBox.Clear();
+            encodeButton.Enabled = true;
+            decodeButton.Enabled = false;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void encodeButton_Click(object sender, EventArgs e)
+        {
+            frequencies = GetCharFrequenciesFromFile(inputFilePath);
+            codingTable = GetCodingTableFromFrequencies(frequencies);
+
+            List<String> encodedStrings = new List<String>();
+
+            Int32 outputSize = 0;
+
+            List<String> encodedTextBoxLines = new List<String>();
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                encodedStrings.Add(EncodeStringWithCodingTable(inputText[i], codingTable));
+                outputSize += encodedStrings.Last().Length;
+            }
+
+            encodedText = encodedStrings.ToArray();
+
+            encodedTextBox.Lines = encodedStrings.ToArray();
+
+            foreach (Char c in frequencies.Keys.ToArray())
+            {
+                dataGridView1.Rows.Add(c, codingTable[c], frequencies[c]);
+            }
+
+            encodedTextLabel.Text = "Encoded text: " + outputSize / 8 + " bytes (compress rate " + (int)((1 - ((double)(outputSize / 8) / inputSize)) * 100) + "%)";
+            encodeButton.Enabled = false;
+            decodeButton.Enabled = true;
+        }
+
+        private void decodeButton_Click(object sender, EventArgs e)
+        {
+            List<String> decodedText = new List<string>();
+            Int32 decodedSize = 0;
+
+            for (int i = 0; i < encodedText.Length; i++)
+            {
+                decodedText.Add(DecodeStringWithCodingTable(encodedText[i], codingTable));
+                decodedSize += decodedText.Last().Length;
+            }
+
+            decodedTextBox.Lines = decodedText.ToArray();
+
+            decodedTextLabel.Text = "Decoded text: " + decodedSize + " bytes";
+            decodeButton.Enabled = false;
+        }
+
+        #region Logic
+
+        
+        static List<String> GetCodesFromJacksonCounter(int count)
+        {
+            List<String> codes = new List<string>();
+
+            int currentCodeLength = 1;
+
+            for (int i = 0; i < count; i++)
+            {
+                String code = new String('0', currentCodeLength);
+
+                for (int j = 0; j < code.Length + 1; j++)
+                {
+                    if ((code.Length < divider.Length || code.Substring(0, divider.Length) != divider) && codes.Count < count)
+                    {
+                        codes.Add(code);
+                        code = shiftString(code);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (!(codes.Count < count))
+                {
+                    break;
+                }
+
+                currentCodeLength++;
+            }
+
+            return codes;
+        }
+
+        static String shiftString(String s)
+        {
+            String result = "1";
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                result += s[i - 1];
+            }
+
+            return result;
+        }
+
+        static String EncodeStringWithCodingTable(String s, Dictionary<Char, String> codingTable)
+        {
+            String result = "";
+
+            if (s.Length == 0)
+            {
+                s = "\n";
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += codingTable[s[i]];
+
+                if (i != s.Length - 1)
+                {
+                    result += divider;
+                }
+            }
+
+            return result;
+        }
+
+        static String DecodeStringWithCodingTable(String s, Dictionary<Char, String> codingTable)
+        {
+            String result = "";
+
+            String[] encodedCharacters = Regex.Split(s, divider);
+
+            foreach (String encodedCharacter in encodedCharacters)
+            {
+                if (codingTable.ContainsValue(encodedCharacter))
+                {
+                    foreach (Char c in codingTable.Keys.ToArray())
+                    {
+                        if (codingTable[c] == encodedCharacter)
+                        {
+                            result += c;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static Dictionary<Char, String> GetCodingTableFromFrequencies(Dictionary<Char, Double> frequencies)
+        {
+            Dictionary<Char, String> codingTable = new Dictionary<Char, String>();
+
+            List<String> codes = GetCodesFromJacksonCounter(frequencies.Keys.Count);
+
+            List<KeyValuePair<Char, Double>> sortedFrequencies = (from entry in frequencies orderby entry.Value descending select entry).ToList();
+
+            for (int i = 0; i < sortedFrequencies.Count; i++)
+            {
+                codingTable[sortedFrequencies[i].Key] = codes[i];
+            }
+
+            return codingTable;
+        }
+
+        static Dictionary<Char, Double> GetCharFrequenciesFromFile(String path)
+        {
+            Dictionary<Char, Double> frequencies = new Dictionary<Char, Double>();
+
+            StreamReader stream = File.OpenText(path);
+            Int32 streamLength = 0;
+
+            while (!stream.EndOfStream)
+            {
+                String line = stream.ReadLine();
+
+                if (line.Length == 0)
+                {
+                    line = "\n";
+                }
+
+                streamLength += line.Length;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    Char c = line[i];
+
+                    if (frequencies.ContainsKey(c))
+                    {
+                        frequencies[c] = frequencies[c] + 1;
+                    }
+                    else
+                    {
+                        frequencies[c] = 1;
+                    }
+                }
+            }
+
+            stream.Close();
+
+            Array keys = frequencies.Keys.ToArray();
+
+            foreach (Char c in keys)
+            {
+                frequencies[c] = frequencies[c] / (Double)streamLength;
+            }
+
+            return frequencies;
+        }
+
+        static Dictionary<Char, Double> GetCharFrequenciesFromString(String s)
+        {
+            Dictionary<Char, Double> frequencies = new Dictionary<Char, Double>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Char c = s[i];
+
+                if (frequencies.ContainsKey(c))
+                {
+                    frequencies[c] = frequencies[c] + 1;
+                }
+                else
+                {
+                    frequencies[c] = 1;
+                }
+            }
+
+            Array keys = frequencies.Keys.ToArray();
+
+            foreach (Char c in keys)
+            {
+                frequencies[c] = frequencies[c] / (Double)s.Length;
+            }
+
+            return frequencies;
+        }
+
+        static String NormalizedString(String s)
+        {
+            String result = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Char c = s[i];
+
+                if ((c >= 'а' && c <= 'я') || (c >= '0' && c <= '9'))
+                {
+                    result += c;
+                }
+                else if (c >= 'А' && c <= 'Я')
+                {
+                    result += (Char)(c - ('А' - 'а'));
+                }
+            }
+    
+            return result;
+        }
+
+        #endregion
+    }
+}
